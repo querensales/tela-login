eaeda9094353f13f0df0081abed7af025377f515
Queren Sales <querenhsales@gmail.com>
2024-07-09 18:35:17 -0300

    corrigindo Js

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ document.addEventListener('DOMContentLoaded', function() {
     var passwordError = document.getElementById('passwordError');
     var message = document.getElementById('message');
 
-    entrarBtn.addEventListener('click', function(event) {
-
+    entrarBtn.addEventListener('click', function() {
         var valid = true;
+
         emailError.textContent = '';
         passwordError.textContent = '';
         message.textContent = '';
 
         if (emailInput.value.trim() === '') {
-            emailError.textContent = 'Por favor, preencha o campo Email.';
+            emailError.textContent = 'Preencha o campo Email.';
             valid = false;
         } else if (!validateEmail(emailInput.value.trim())) {
-            emailError.textContent = 'Por favor, insira um endereço de email válido.';
+            emailError.textContent = 'Insira um endereço de email válido.';
             valid = false;
         }
 
-        // Verifica se o campo de senha está vazio
         if (passwordInput.value.trim() === '') {
-            passwordError.textContent = 'Por favor, preencha o campo Senha.';
+            passwordError.textContent = 'Preencha o campo Senha.';
             valid = false;
         }
 
         if (!valid) {
-            message.textContent = 'Por favor, corrija os erros antes de enviar.';
+            message.textContent = '';
             message.className = 'error';
         } else {
             message.textContent = 'Login realizado com sucesso!';
             message.className = 'success';
+        
         }
     });
 
